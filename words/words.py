9303b3fadcd13f7267207e62c826410c0ab272a1
Ángel Guillermo Ortiz González <30931826+AngelGuillermoOrtizGonzalez@users.noreply.github.com>
2021-09-27 21:18:26 -0500

    Añadido py para palabras

---
+++ words/words.py
@@ -0,0 +1,5 @@
+print("Welcome to the program that receives words separated by spaces and prints them one by one")
+sentence=input("Input all the words separated by spaces: ")
+words=sentence.split()
+print(*words, sep="\n")
+
